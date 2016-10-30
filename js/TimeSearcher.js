aaab706e5e63bb8c7a9d5b5e5032790e71d992c4
Jack Swiggett <jackswiggett@gmail.com>
2016-10-29 19:39:42 -0700

    TimeSearcher.js: graph values for all the stocks

--- js/TimeSearcher.js
+++ js/TimeSearcher.js
@@ -7,36 +7,51 @@ d3.csv("data/stock_data.csv", function(error, data) {
     if (error) {
         console.log(error);
     } else {
-        stock_names = data.columns;
-        stock_names.shift(); // remove "date" column
+        // convert stock data into a format that is easy to visualize
+        var stocks = data.columns;
+        stocks.shift(); // remove "date" column
 
-        // for now, we just graph the first stock
-        name = stock_names[0];
-        values = data.map(function (d) {
-            return { 
-                date: new Date(d.date),
-                value: Number(d[name])
-            };
+        stocks = stocks.map(function(stock_name) {
+            return data.map(function (d) {
+                return {
+                    date: new Date(d.date),
+                    value: Number(d[stock_name])
+                };
+            });
         });
 
-        timeScale = d3.scaleTime()
-            .domain(d3.extent(values, d => d.date))
+        // create time scale and value scale to map raw data to screen positions
+        var timeScale = d3.scaleTime()
+            .domain([
+                new Date(stocks[0][0].date),
+                new Date(stocks[0][stocks[0].length - 1].date)
+            ])
             .range([100, 700]);
 
-        valueScale = d3.scaleLinear()
-            .domain([0, d3.max(values, d => d.value)])
+        var maxStockValue = d3.max(stocks, function(stock) {
+            return d3.max(stock, function(d) {
+                return d.value;
+            });
+        });
+
+        var valueScale = d3.scaleLinear()
+            .domain([0, maxStockValue])
             .range([400, 100]);
 
-        var line = d3.line()
+        // create and display SVG paths for all the stocks
+        var generatePathCoords = d3.line()
             .x(function(d) { return timeScale(d.date); })
             .y(function(d) { return valueScale(d.value); });
 
-        svg.append("path")
-            .datum(values)
-            .attr("d", line)
+        var lines = svg.selectAll(".stock-line")
+            .data(stocks);
+
+        lines.enter().append("path")
+            .attr("d", generatePathCoords)
             .attr("stroke", "black")
             .attr("stroke-width", 1)
-            .attr("fill", "none");
+            .attr("fill", "none")
+            .attr("class", "stock-line");
 
     }
 });
